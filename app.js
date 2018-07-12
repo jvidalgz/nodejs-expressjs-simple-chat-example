8145d07c5693c305f1ba7135e7fe2b2b7e8c7a1f
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2018-07-12 17:57:46 -0400

    Estructura basica de aplicacion

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
-
+var rooms = require("./data/rooms.json")
+app.set('views', './views')
+app.set('view engine', 'pug')
 // middleware para retornar archivos estáticos (http://expressjs.com/es/starter/static-files.html)
 app.use(express.static('public'))
 app.use(express.static('node_modules/bootstrap/dist'))
 
-app.get('/hola', function(req, res){
-  res.send('Hola desde expressjs')
+app.get('/', function(req, res){
+    res.render('index')
+})
+
+app.get('/admin/rooms', function(req, res){
+  res.render('rooms', {
+      title: 'Admin Rooms',
+      rooms:rooms
+  })
 })
 
 app.listen(3000, function(){
