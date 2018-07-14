21e430b3e8a070d8b923eb74000df1cc69ef75c0
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2018-07-14 11:37:32 -0400

    Agrupa rutas bajo '/admin'

--- admin.js
+++ admin.js
@@ -6,21 +6,21 @@ let rooms = require("./data/rooms.json")
 const router = express.Router()
 module.exports = router
 
-router.get('/admin/rooms', function (req, res) {
+router.get('/rooms', function (req, res) {
     res.render('rooms', {
         title: 'Admin Rooms',
         rooms: rooms
     })
 })
 
-router.get('/admin/rooms/add', function (req, res) {
+router.get('/rooms/add', function (req, res) {
     res.render('add')
 })
-router.get('/admin/rooms/edit/', function (req, res) {
+router.get('/rooms/edit/', function (req, res) {
     res.render('edit')
 })
 
-router.post('/admin/rooms/add', function (req, res) {
+router.post('/rooms/add', function (req, res) {
     const room = {
         name: req.body.name,
         id: uuid.v4()
@@ -30,7 +30,7 @@ router.post('/admin/rooms/add', function (req, res) {
     res.redirect('/admin/rooms')
 })
 
-router.post('/admin/rooms/edit/:id', function (req, res) {
+router.post('/rooms/edit/:id', function (req, res) {
     const roomId = req.params.id
     let room = _.find(rooms, r => r.id === roomId)
     if (!room) {
@@ -42,13 +42,13 @@ router.post('/admin/rooms/edit/:id', function (req, res) {
     res.redirect('/admin/rooms')
 })
 
-router.get('/admin/rooms/delete/:id', function (req, res) {
+router.get('/rooms/delete/:id', function (req, res) {
     const roomId = req.params.id
     rooms = rooms.filter(r => r.id !== roomId)
     res.redirect('/admin/rooms')
 })
 
-router.get('/admin/rooms/edit/:id', function (req, res) {
+router.get('/rooms/edit/:id', function (req, res) {
     const roomId = req.params.id
     const room = _.find(rooms, r => r.id === roomId)
     if (!room) {
