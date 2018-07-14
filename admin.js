9ccca539b0d72c2f3067db0862030d4cbbf0fed5
Jaime Vidal Gonzalez <jvidalgz@gmail.com>
2018-07-14 11:59:10 -0400

    Chaining routes

--- admin.js
+++ admin.js
@@ -13,47 +13,48 @@ router.get('/rooms', function (req, res) {
     })
 })
 
-router.get('/rooms/add', function (req, res) {
-    res.render('add')
+router.route('/rooms/add')
+    .get(function (req, res) {
+        res.render('add')
+    })
+    .post(function (req, res) {
+        const room = {
+            name: req.body.name,
+            id: uuid.v4()
+        }
+        rooms.push(room)
+
+        res.redirect(`${req.baseUrl}/rooms`)
 })
+
 router.get('/rooms/edit/', function (req, res) {
     res.render('edit')
 })
 
-router.post('/rooms/add', function (req, res) {
-    const room = {
-        name: req.body.name,
-        id: uuid.v4()
-    }
-    rooms.push(room)
 
-    res.redirect(`${req.baseUrl}/rooms`)
-})
+router.route('/rooms/edit/:id')
+    .get(function (req, res) {
+        const roomId = req.params.id
+        const room = _.find(rooms, r => r.id === roomId)
+        if (!room) {
+            res.sendStatus(404)
+            return
+        }
+        res.render('edit', {room})
+    }).post(function (req, res) {
+        const roomId = req.params.id
+        let room = _.find(rooms, r => r.id === roomId)
+        if (!room) {
+            res.sendStatus(404)
+            return
+        }
+        room.name = req.body.name
 
-router.post('/rooms/edit/:id', function (req, res) {
-    const roomId = req.params.id
-    let room = _.find(rooms, r => r.id === roomId)
-    if (!room) {
-        res.sendStatus(404)
-        return
-    }
-    room.name = req.body.name
-
-    res.redirect(`${req.baseUrl}/rooms`)
+        res.redirect(`${req.baseUrl}/rooms`)
 })
 
 router.get('/rooms/delete/:id', function (req, res) {
     const roomId = req.params.id
     rooms = rooms.filter(r => r.id !== roomId)
     res.redirect(`${req.baseUrl}/rooms`)
-})
-
-router.get('/rooms/edit/:id', function (req, res) {
-    const roomId = req.params.id
-    const room = _.find(rooms, r => r.id === roomId)
-    if (!room) {
-        res.sendStatus(404)
-        return
-    }
-    res.render('edit', {room})
 })
